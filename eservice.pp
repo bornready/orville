d94805bbf544df1768b6abfa65651289da15cd94
George Richmond <georgerichmond@gmail.com>
2011-09-13 15:03:43 +0100

    added rvm stuff

--- eservice.pp
+++ eservice.pp
@@ -15,3 +15,51 @@ service {"httpd":
 	require => Package["httpd"],
 	ensure => running,
 }
+
+package {"subversion":
+	ensure => installed,
+	}
+
+package {"git":
+	ensure => installed,
+	}
+
+package {"maven":
+	ensure => installed,
+	}
+
+package {"curl":
+	ensure => installed,
+	}
+
+
+user { "user":
+                    home    => "/home/user",
+            }
+
+
+import "rvm"
+
+include rvm::system
+
+rvm_system_ruby {
+    'jruby':
+      ensure => 'present',
+      default_use => true;	
+  }
+
+rvm::system_user {user: ;}
+
+rvm_gem {
+    'jruby@global/bundler':
+  }
+
+augeas{"BUILD_NUMBER":
+		changes=> [
+		"set BUILD_NUMBER LOCAL"
+		]
+	} 
+
+
+
+
