4c0e5e39402d7dc74ba35df643e4b4f60e25b9a7
user <user@localhost.localdomain>
2011-09-15 12:36:09 +0100

    added wget module

--- init.pp
+++ init.pp
@@ -1,14 +1,4 @@
-class wget {
-	package { "wget": ensure => latest }
-}
-
-
-define wget::fetch($source,$destination) {
-	exec { "wget-$name":
-		command => "/usr/bin/wget --output-document=$destination $source",
-		creates => "$destination",
-	}
-}
+include wget
 
 import 'jdk.pp'
 class {'jdk': }
