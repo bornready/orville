206e9aeb9814b6610afd699b2e8207710a8c6d2f
user <user@localhost.localdomain>
2011-09-15 14:59:47 +0100

    think we have sorted out wget require problem hopefully

--- modules/wget/manifests/init.pp
+++ modules/wget/manifests/init.pp
@@ -7,6 +7,7 @@ define wget::fetch($source,$destination) {
 	exec { "wget-$name":
 		command => "/usr/bin/wget --output-document=$destination $source",
 		creates => "$destination",
+		require => Package['wget'],
 	}
 }
 
