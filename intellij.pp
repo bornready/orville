c2c1693142de1e669f2581b151d2538e61f0a4bc
user <user@localhost.localdomain>
2011-09-16 17:09:58 +0100

    BDO|JB: Ensure intellij is not unpacked every time

--- intellij.pp
+++ intellij.pp
@@ -1,6 +1,7 @@
 class intellij{
 	
 	$idea_version = "ideaIU-10.5.2"
+	$idea_build_number = "idea-IU-107.587"
 	$intellij_url = "http://download.jetbrains.com/idea/$idea_version.tar.gz"
 	$intellij_install_file = "/usr/tmp/idea.tar.gz"
 
@@ -19,8 +20,8 @@ class intellij{
 	exec {"unpack_intellij":
 		require => Wget::Fetch["intellij_install"],		
 		cwd =>"/home/user/Applications",	
-		command=>"/bin/tar -xvf $intellij_install_file;ln -s idea* idea;chown -R user:user idea*",
-		creates=>"/home/user/Applications/$idea_version",
+		command=>"/bin/tar -xvf $intellij_install_file;ln -s $idea_build_number idea;chown -R user:user idea $idea_build_number",
+		creates=>"/home/user/Applications/$idea_build_number/",
 	}
 
 
