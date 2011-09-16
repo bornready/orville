477e0143e7fac2aff3f2f97007d035662878de30
user <user@localhost.localdomain>
2011-09-16 16:53:35 +0100

    JB|BDO: moving intellij to user folder

--- intellij.pp
+++ intellij.pp
@@ -1,10 +1,7 @@
-
-import 'augeas.pp'
 class intellij{
 	
-include augeas
-
-	$intellij_url = "http://download.jetbrains.com/idea/ideaIU-10.5.2.tar.gz"
+	$idea_version = "ideaIU-10.5.2"
+	$intellij_url = "http://download.jetbrains.com/idea/$idea_version.tar.gz"
 	$intellij_install_file = "/usr/tmp/idea.tar.gz"
 
 	wget::fetch{"intellij_install":
@@ -13,20 +10,26 @@ include augeas
 	}
 
 
+	file{"/home/user/Applications":
+		ensure => directory,
+		owner => "user",
+		group => "user"
+	}
+
 	exec {"unpack_intellij":
 		require => Wget::Fetch["intellij_install"],		
-		cwd =>"/opt",	
-		command=>"/bin/tar -xvf $intellij_install_file;mv idea* idea",
-		creates=>"/opt/idea/bin/",
+		cwd =>"/home/user/Applications",	
+		command=>"/bin/tar -xvf $intellij_install_file;ln -s idea* idea;chown -R user:user idea*",
+		creates=>"/home/user/Applications/$idea_version",
 	}
 
 
 	file {"/etc/profile.d/intellij.sh":
-		ensure => "present",
-		owner => "root",
-		group => "root",
+		ensure => present,
+		owner => "user",
+		group => "user",
 		mode => 644,
-		content => "PATH=\$PATH:/opt/idea/bin",
+		content => "PATH=\$PATH:~/Applications/idea/bin",
         }
 
 	
