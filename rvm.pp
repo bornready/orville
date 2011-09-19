5ee1f977fd018196d0a7040fa7fbbf3dc727cbb6
user <user@localhost.localdomain>
2011-09-19 14:41:52 +0100

    AG|BDO: continuing work on rvm

--- rvm.pp
+++ rvm.pp
@@ -2,19 +2,8 @@ package{["bash", "curl", "git", "libxslt", "gcc-c++", "patch", "readline", "read
 	ensure =>installed,
 }
 
-bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
-
-  
-  If you wish to use RVM in an interactive fashion in your shells then
-  Place the following line at the end of your shell's loading files
-  (.bashrc or .bash_profile for bash and .zshrc for zsh),
-  after all PATH/variable settings:
-
-  [[ -s "/home/user/.rvm/scripts/rvm" ]] && source "/home/user/.rvm/scripts/rvm"  # This loads RVM into a shell session.
-
-  You only need to add this line the first time you install RVM.
-
-  If you are choosing to source RVM into your environment to switch current
-  shell environments, be sure to close this shell and open a new one so that
-  the RVM functions load.
+exec { "rvm":
+    command => "/usr/bin/sudo -u user /bin/bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)",
+    creates => "/home/user/.rvm/bin/rvm"
+}
 
