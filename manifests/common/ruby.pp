062583186147cdb25b573c672e8a347d0117ca57
user <user@localhost.localdomain>
2011-09-20 15:08:08 +0100

    BDO|GR: Added dependencies of rvm and ruby-1.9.2

--- manifests/common/ruby.pp
+++ manifests/common/ruby.pp
@@ -2,8 +2,15 @@ import 'rvm'
 
 include rvm::system
 
+$rvm_dependencies = ["bash", "curl", "git", "libxslt"]
+$ruby_dependencies = ["gcc-c++", "patch", "readline", "readline-devel", "zlib", "zlib-devel", "libyaml-devel", "libffi-devel", "openssl-devel", "make", "bzip2", "iconv-devel"]
+
 rvm::system_user {user: 
-	require => User["user"];
+	require => [User["user"],Package["$rvm_dependencies"]];
+}
+
+package { $rvm_dependencies: 
+	ensure => installed
 }
 
 package { $ruby_dependencies: 
@@ -20,6 +27,7 @@ if $rvm_installed == "true" {
 	rvm_system_ruby {'ruby-1.9.2-p290':
 		ensure => present,
 		default_use => false,
+		require => Package["$ruby_dependencies"]
 	}
 
 	rvm_gem {'ruby-1.9.2-p290/bundler':
