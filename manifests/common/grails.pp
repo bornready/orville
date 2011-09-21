16b6d25136be6b56a88188bef0239255fde6b494
user <user@localhost.localdomain>
2011-09-21 16:09:20 +0100

    MS|BDO: Starting to work on the /apps directory

--- manifests/common/grails.pp
+++ manifests/common/grails.pp
@@ -9,7 +9,7 @@ file {"/etc/profile.d/grails.sh":
 	owner => "root",
 	group => "root",
 	mode => 644,
-	content => "export GRAILS_HOME=/usr/grails/default;PATH=\$PATH:\$GRAILS_HOME/bin",
+	content => 'export GRAILS_HOME=/usr/grails/default;PATH=$PATH:$GRAILS_HOME/bin',
 }
 
 $grails_install_path = "/usr/grails"
@@ -31,19 +31,4 @@ exec {"unpack_grails":
 	command=>"/usr/bin/unzip $grails_download; ln -s $grails_version default",
 	creates=>"$grails_install_path/$grails_version/",
 }
-
-file {"/etc/profile.d/groovy.sh":
-	ensure => "present",
-	owner => "root",
-	group => "root",
-	mode => 644,
-	content => "export GROOVY_HOME=$grails_install_path/default;PATH=\$PATH:\$GROOVY_HOME/bin",
-}
-
-fetch {"groovy":		
-	source => "http://dist.groovy.codehaus.org/distributions/groovy-binary-1.8.2.zip",
-	destination => "/tmp/groovy.zip",
-}
-
-
 
